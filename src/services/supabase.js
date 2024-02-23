7bde4b51ca70ca1bd5ad188760d9e431d10619a3
Irina Saveleva <goyalion17@gmail.com>
2024-02-23 20:37:23 +0100

    editing form

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://qfqtglzrdokobprwzndt.supabase.co";
+export const supabaseUrl = "https://qfqtglzrdokobprwzndt.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmcXRnbHpyZG9rb2Jwcnd6bmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0NDE4NzcsImV4cCI6MjAyNDAxNzg3N30.RikFJ4H2a-OcHaV1w-rXXhl-EZkI6txc_wQIHSagauM";
 const supabase = createClient(supabaseUrl, supabaseKey);
