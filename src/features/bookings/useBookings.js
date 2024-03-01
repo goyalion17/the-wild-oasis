41e05ad5989f24c47c340f96343414c7f4f55142
Irina Saveleva <goyalion17@gmail.com>
2024-03-01 18:27:55 +0100

    api-side sorting booking

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -11,15 +11,20 @@ export function useBookings() {
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
-      // : { field: "totalPrice", value: 5000, method : 'gte' };
+  // : { field: "totalPrice", value: 5000, method : 'gte' };
+
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
 
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
