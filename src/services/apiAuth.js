47ef7a721305cf91e910fb4d50c0f1e586e24e19
Irina Saveleva <goyalion17@gmail.com>
2024-03-05 18:16:13 +0100

    user logout

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -7,7 +7,6 @@ export async function login({ email, password }) {
   });
 
   if (error) throw new Error(error.message);
-
   return data;
 }
 
@@ -17,9 +16,12 @@ export async function getCurrentUser() {
 
   const { data, error } = await supabase.auth.getUser();
 
-  console.log(data);
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
-
-  return data?.user;
 }
