5b5d5c7d8af6bee866892bbd52e84d19c68b9fa8
Irina Saveleva <goyalion17@gmail.com>
2024-03-05 20:21:07 +0100

    user sign up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,15 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-
-// Email regex: /\S+@\S+\.\S+/
+import { useSignup } from "./useSignup";
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password }, { onSettled: () => reset() });
   }
 
   return (
@@ -20,6 +20,7 @@ function SignupForm() {
         <Input
           type="text"
           id="fullName"
+          disabled={isLoading}
           {...register("fullName", { required: "this field is required" })}
         />
       </FormRow>
@@ -28,6 +29,7 @@ function SignupForm() {
         <Input
           type="email"
           id="email"
+          disabled={isLoading}
           {...register("email", {
             required: "this field is required",
             pattern: {
@@ -45,6 +47,7 @@ function SignupForm() {
         <Input
           type="password"
           id="password"
+          disabled={isLoading}
           {...register("password", {
             required: "this field is required",
             minLength: {
@@ -59,6 +62,7 @@ function SignupForm() {
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isLoading}
           {...register("passwordConfirm", {
             required: "this field is required",
             validate: (value) =>
@@ -69,10 +73,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -4,7 +4,7 @@ import Heading from "../ui/Heading";
 function NewUsers() {
   return (
     <>
-      return <Heading as="h1">Create a new user</Heading>
+      <Heading as="h1">Create a new user</Heading>
       <SignupForm />
     </>
   );
