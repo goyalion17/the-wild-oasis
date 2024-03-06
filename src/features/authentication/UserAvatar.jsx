33e6a91840e6990927330281c70e9811d06417b9
Irina Saveleva <goyalion17@gmail.com>
2024-03-06 18:20:32 +0100

    building app header

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUser";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -11,7 +12,7 @@ const StyledUserAvatar = styled.div`
 
 const Avatar = styled.img`
   display: block;
-  width: 4rem;
+  /* width: 4rem; */
   width: 3.6rem;
   aspect-ratio: 1;
   object-fit: cover;
@@ -19,3 +20,18 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+export default function UserAvatar() {
+  const { user } = useUser();
+  const { fullname, avatar } = user.user_metadata;
+
+  return (
+    <StyledUserAvatar>
+      <Avatar
+        src={avatar || "default-user.jpg"}
+        alt={`Avatar of ${fullname}`}
+      />
+      <span>{fullname}</span>
+    </StyledUserAvatar>
+  );
+}
