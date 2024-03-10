6b83b38df776f5cf072e26f0837cf2ea91d99f69
Irina Saveleva <goyalion17@gmail.com>
2024-03-10 14:10:47 +0100

    updating AddCabin

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,6 +1,5 @@
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
-import CabinTable from "./CabinTable";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
@@ -17,20 +16,5 @@ function AddCabin() {
     </div>
   );
 }
-// export default function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
 
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((show) => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}  />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 export default AddCabin;
